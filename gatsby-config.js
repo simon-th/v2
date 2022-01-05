69116308ce84950203c2557bbd9f1b1edf8f94c2
Simon Hoque <simon.thoque@gmail.com>
2022-01-05 05:47:30 +0000

    Add Tailwind and basic intro

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     siteUrl: 'https://www.yourdomain.tld',
     title: 'Gatsby Starter',
   },
-  plugins: [],
+  plugins: [`gatsby-plugin-postcss`],
 };
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    container: {
+      padding: {
+        DEFAULT: '2rem',
+        md: '5rem',
+        lg: '18rem',
+      },
+    },
+  },
+  plugins: [],
+};

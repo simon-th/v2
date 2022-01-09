53b6c62b781f71023e6fc7bc41dfa78cf9c63b36
Simon Hoque <simon.thoque@gmail.com>
2022-01-09 02:43:34 +0000

    Add style helper files

--- src/styles/common-styles.ts
+++ src/styles/common-styles.ts
@@ -1,3 +1,6 @@
-export const paragraphClass = 'text-md md:text-lg xxl:text-xl mb-4';
-export const linkHoverEffectClass =
+export const h1 = 'text-2xl md:text-3xl font-bold';
+export const h2 = 'text-xl md:text-2xl font-semibold';
+export const h3 = 'text-lg md:text-xl font-semibold';
+export const p = 'text-md md:text-lg xxl:text-xl';
+export const linkHoverEffect =
   'relative after:absolute after:bg-gray-400 after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-left after:scale-x-100 hover:after:origin-bottom-right hover:after:scale-x-0 after:transition-transform after:ease-in-out after:duration-300';

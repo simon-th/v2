f758dc70a596ee608e55cd92b85524975f4099b9
Simon Hoque <simon.thoque@gmail.com>
2022-01-10 06:38:02 +0000

    Fix navbar hamburger menu highlight

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -46,7 +46,7 @@ function MobileMenuButton(props: MobileMenuButtonProps) {
     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
       <button
         type="button"
-        className="inline-flex items-center justify-center p-2 rounded-md text-slate-400 hover:text-white hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+        className="inline-flex items-center justify-center p-2 rounded-md text-slate-400 hover:text-white hover:bg-highlight focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
         aria-controls="mobile-menu"
         aria-expanded="false"
         onClick={onClick}
